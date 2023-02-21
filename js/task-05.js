a84f75d87816bffe251d42a02d03c63e043b9775
Maria Shymanska <114136940+Maria-Shymanska@users.noreply.github.com>
2023-02-21 16:48:19 +0100

    update 9 task

--- js/task-05.js
+++ js/task-05.js
@@ -1,3 +1,6 @@
+// 1. Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input), підставляє його поточне значення в span#name-output.
+//  Якщо інпут порожній, у спані повинен відображатися рядок "Anonymous".
+
 const inputName = document.querySelector("#name-input");
 const spanName = document.querySelector("#name-output");
 
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,5 @@
+// Напиши скрипт, який змінює кольори фону елемента <body> через інлайн-стиль по кліку на button.change-color і виводить значення кольору в span.color.
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
@@ -10,5 +12,5 @@ btnElement.addEventListener("click", ChangeColorClick);
 
 function ChangeColorClick() {
   bodyElement.style.backgroundColor = getRandomHexColor();
-  spanElement.textContent = bodyElement.style.backgroundColor;
+  spanElement.textContent = bodyElement.style.backgroundColor = hexColor;
 }
